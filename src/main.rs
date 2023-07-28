d710943597a8a0b471e182d7f430d5b5da7b745f
karegoie <karegoie@snu.ac.kr>
2023-07-28 12:15:41 +0900

    230728 parallelizing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use bio::io::fastq;
+use std::sync::{Arc, Mutex};
+use rayon::prelude::*;
 
 fn main() {
     let binding = std::env::args().nth(1).unwrap();
@@ -18,25 +20,31 @@ fn main() {
     let file = std::fs::File::open(path).unwrap();
     let reader = std::io::BufReader::new(file);
     // check whether the file is fasta or fastq
-    let mut writer = fastq::Writer::to_file("./result/filtered.fastq").unwrap();
-    let mut cnt = 0;
-    for record in fastq::Reader::new(reader).records() {
-        cnt += 1;
-        if cnt % 10000 == 0 {
-            println!("{} reads processed", cnt);
-        }
-        let record = record.unwrap();
-        if record.seq().len() < params["min_read_length"].parse::<usize>().unwrap() {
-            continue;
-        }
-        let seq = record.seq().to_vec();
+    let one_writer = fastq::Writer::to_file("./result/filtered.fastq").unwrap();
+    let writer = Arc::new(Mutex::new(one_writer));
 
-        let sig_seq = clean_wrf::tosignal::convert_to_signal(&seq);
-        let cwt_seq = clean_wrf::cwt::cwt(sig_seq, &params);
+    // Parallelize the loop using Rayon's par_iter.
+    fastq::Reader::new(reader)
+        .records()
+        .par_iter()  // Use par_iter() to parallelize the iterator.
+        .enumerate()
+        .for_each(|(cnt, record)| {
+            let record = record.unwrap();
+            if record.seq().len() < params["min_read_length"].parse::<usize>().unwrap() {
+                return;
+            }
+            let seq = record.seq().to_vec();
+            let sig_seq = clean_wrf::tosignal::convert_to_signal(&seq);
+            let cwt_seq = clean_wrf::cwt::cwt(sig_seq, &params);
 
-        if clean_wrf::filter::linear_argmax(&cwt_seq, &params) {
-            writer.write_record(&record).unwrap();
-        }
+            if clean_wrf::filter::linear_argmax(&cwt_seq, &params) {
+                // Lock the writer before writing to it.
+                let mut writer_lock = writer.lock().unwrap();
+                writer_lock.write_record(&record).unwrap();
+            }
 
-    }
+            if cnt % 10000 == 0 {
+                println!("{} reads processed", cnt);
+            }
+        });
 }
